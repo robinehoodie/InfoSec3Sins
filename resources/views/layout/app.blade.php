b3f57b09e9a5f3f383fb17420b2bd875d4225402
robinehoodie <robinecole260@gmail.com>
2021-10-02 20:03:02 +0800

    wording

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,5 @@
 {{-- This is the initial page of the website
-    The 3 sins are located:
+    The 3 sins solutions are located in the following:
     1. app/Http/Controllers/Register.php -> Password are encrypted to ensure the user security
     2. Models, Controllers, Middleware and migrations are separated -> Models, Controllers and middleware
         are located in app/http , migrations are located in database
